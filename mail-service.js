13aba331a79de1c5930ce8b12dcfd6ecb12a177d
Harshit Dwivedi <hdwivedi145@gmail.com>
2024-11-10 16:04:47 +0530

    added private  configuration

--- mail-service.js
+++ mail-service.js
@@ -3,18 +3,13 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const handleMailSendService = async(req, res) => {
     try {
-        const { mailId, title, mailBody } = req.body;
-        console.log(mailId, title, mailBody);
-        
+        const { mailId, title, mailBody } = req.body;        
         const res = await resend.emails.send({
-            from: 'workwithus@mailer.com',
+            from: process.env.SEND_MAIL_DOMAIN,
             to: mailId,
             subject: title,
             html: mailBody
         });
-
-        console.log("res", res);
-        
         return res.status(200).send({ message: "Mail Sent Successfully" });
     } catch (error) {
         return res.status(404).send({ message: `Error in mail - ${error}` });
